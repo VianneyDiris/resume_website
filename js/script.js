9ef93f07543d45a8927077263f5988c0a7ac918d
vianney diris <vianney.diris@gmail.com>
2020-05-01 15:20:45 +0200

    add heading and message

--- js/script.js
+++ js/script.js
@@ -4,4 +4,12 @@ $(document).ready(function() {
         play : 5000,
         pagination : false
     });
+
+    var typed = new Typed(".typed",{
+        strings : ["développeur java","alternant devops ?","futur chef de projet"],
+        typeSpeed : 70,
+        loop : true,
+        startDelay : 1000,
+        showCursor : false 
+    });
 });
